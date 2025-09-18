8750b8520e54a7811d1648bee245a4ed4b72c153
JackVlg <jackvlgv@gmail.com>
2025-09-18 22:07:22 +0300

    Fixed search for directories

--- platform/directories/built-in-directories/src/main/java/tech/eebase/directories/Currency.java
+++ platform/directories/built-in-directories/src/main/java/tech/eebase/directories/Currency.java
@@ -2,9 +2,20 @@ package tech.eebase.directories;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import tech.eebase.directories.annotations.Directory;
+import tech.eebase.directories.annotations.DirectoryType;
 
 @Entity
 @Table(name = "DIC_CURRENCIES")
+@Directory(
+        type=DirectoryType.FLAT,
+        key="currencies",
+        singularName="Currency",
+        pluralName="Currencies",
+        createTitleName="Create currency",
+        editTitleName="Edit currency",
+        icon="fa fa-cube",
+        weight=100)
 public class Currency extends DirectoryBase {
 
 }
--- platform/directories/directories-internal-api/src/main/java/tech/eebase/directories/annotations/Directory.java
+++ platform/directories/directories-internal-api/src/main/java/tech/eebase/directories/annotations/Directory.java
@@ -1,5 +1,12 @@
 package tech.eebase.directories.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
 public @interface Directory {
     DirectoryType type();
     String key();
